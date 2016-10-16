9aea8df5210efd1b5caf24726cbfdb7b510960b3
swaption2009 <swaption2009@gmail.com>
2016-10-15 22:59:29 -0700

    search box observable

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
@@ -12,6 +14,7 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroSearchComponent }  from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -27,7 +30,8 @@ import { AppRoutingModule }     from './app-routing.module';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
